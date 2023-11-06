1e2aa7beb86063db6660f7ea1838150fb80d1331
pooulad <poooooladi@gmail.com>
2023-11-06 21:40:55 +0330

    create HandleRoot func

--- pkg/url_shortner.go
+++ pkg/url_shortner.go
@@ -20,6 +20,30 @@ func NewUrlShortner(urls map[string]string) *UrlShortner {
 	}
 }
 
+func (u *UrlShortner) HandleRoot(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		http.Redirect(w, r, "/shorten", http.StatusSeeOther)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>gurl-shortner</title>
+			</head>
+			<body>
+				<h2>gurl-shortner</h2>
+				<form method="post" action="/shorten">
+					<input type="url" name="url" placeholder="Enter a URL" required>
+					<input type="submit" value="Shorten">
+				</form>
+			</body>
+			</html>
+		`)
+}
+
 func (u *UrlShortner) HandleShortenUrl(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)

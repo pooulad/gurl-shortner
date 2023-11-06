a298079cc5b6caeb1d3a162f6194229f0046226c
pooulad <poooooladi@gmail.com>
2023-11-06 21:35:24 +0330

    handle /shorten

--- pkg/url_shortner.go
+++ pkg/url_shortner.go
@@ -13,6 +13,29 @@ func NewUrlShortner(urls map[string]string) *UrlShortner {
 }
 
 func (u *UrlShortner) HandleShortenUrl(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	originalURL := r.FormValue("url")
+	if originalURL == "" {
+		http.Error(w, "URL parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	shortKey := shortnerKeyGenerator()
+	u.urls[shortKey] = originalURL
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	host := os.Getenv("HOST_ADDR")
+	port := os.Getenv("HOST_PORT")
+
+	shortenedURL := fmt.Sprintf("http://%v:%v/short/%s", host, port, shortKey)
 
 }
 func (u *UrlShortner) HandleRedirectUrl(w http.ResponseWriter, r *http.Request) {}
